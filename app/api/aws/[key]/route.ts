b53b8ab0dc0eced5e64afa6e3b6b98ff1bf29efa
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-01 01:10:44 +0530

    conditional api res.send

--- app/api/aws/[key]/route.ts
+++ app/api/aws/[key]/route.ts
@@ -1,11 +1,22 @@
 import { NextRequest, NextResponse } from "next/server";
 import getPresignedUrl from "@/app/utils/getPresignedUrl";
+import checkObjectExist from "@/app/utils/checkObjectExist";
+
+const bucketName = "clipboard2";
 
 export const GET = async(req: NextRequest, res: NextResponse) => {
-
-    const reqUrl = req.url?.split('/');
-    const key = reqUrl[5];
-    const url = await getPresignedUrl(key);
-
-    return NextResponse.json({ url: url }, { status: 200 });
+    try {
+        const reqUrl = req.url?.split('/');
+        const key = reqUrl[5];
+        const exists = await checkObjectExist(bucketName, key);
+        const url = await getPresignedUrl(key);
+        if (!exists) {
+            return NextResponse.json({ url :"" }, { status: 200 });
+        }
+    return NextResponse.json({ url: url }, { status: 200 }); 
+    } catch (error : any) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+   
 }
+
--- app/utils/checkObjectExist.js
+++ app/utils/checkObjectExist.js
@@ -1,11 +1,11 @@
 import { HeadObjectCommand } from "@aws-sdk/client-s3";
-import { S3Client } from "./s3Client";
+import { s3Client } from "./s3Client";
 
 async function doesObjectExist(bucketName, key) {
   try {
     // Send a HEAD request to the object
     const command = new HeadObjectCommand({ Bucket: bucketName, Key: key });
-      await S3Client.send(command);
+      await s3Client.send(command);
     return true; // Object exists
   } catch (error) {
     if (error.name === "NotFound") {
