2eca332b9feebb023b59923ce8bfdab31cd3b98c
Sanjay Sirangi <sanjaysirangi@gmail.com>
2024-03-02 15:11:42 +0530

    changed utils/checkObject - typescript

--- app/utils/checkObjectExist.ts
+++ app/utils/checkObjectExist.ts
@@ -1,13 +1,23 @@
 import { HeadObjectCommand } from "@aws-sdk/client-s3";
 import { s3Client } from "./s3Client";
 
-async function doesObjectExist(bucketName, key) {
+/**
+ * Checks if an object exists in the specified S3 bucket.
+ * @param bucketName The name of the S3 bucket.
+ * @param key The key (path) of the object in the bucket.
+ * @returns A promise that resolves to true if the object exists, false otherwise.
+ */
+
+async function doesObjectExist(
+  bucketName: string,
+  key: string
+): Promise<boolean> {
   try {
     // Send a HEAD request to the object
     const command = new HeadObjectCommand({ Bucket: bucketName, Key: key });
-      await s3Client.send(command);
+    await s3Client.send(command);
     return true; // Object exists
-  } catch (error) {
+  } catch (error: any) {
     if (error.name === "NotFound") {
       return false; // Object does not exist
     }
